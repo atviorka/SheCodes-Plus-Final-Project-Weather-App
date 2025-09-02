bed5718d25c67cd3e74d5768502fdc8c44614c67
atviorka <120952488+atviorka@users.noreply.github.com>
2025-09-02 18:58:30 +0300

    added more weather data/search JS

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = Math.round(temperature);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  console.log(response.data);
+
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-    let apiKey = "fb2eo2345t09c60c3a4b1c043a90eee3";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    axios.get(apiUrl).then(refreshWeather)
-
+  let apiKey = "fb2eo2345t09c60c3a4b1c043a90eee3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
 
-searchCity(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
