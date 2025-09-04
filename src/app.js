78eda9b1f98bbe5986ec4a41d1ecba7228fee93b
atviorka <120952488+atviorka@users.noreply.github.com>
2025-09-04 13:08:41 +0300

    added HTML & CSS for the forecast

--- src/app.js
+++ src/app.js
@@ -49,4 +49,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Telsiai");
+searchCity("London");
