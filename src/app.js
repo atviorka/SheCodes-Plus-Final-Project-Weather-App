c3879dfbfaee7fd8374dba2708f39e7b312ca5ad
atviorka <120952488+atviorka@users.noreply.github.com>
2025-09-08 14:33:16 +0300

    Injected the forecast HTML from JasvaScript

--- src/app.js
+++ src/app.js
@@ -20,17 +20,24 @@ function refreshWeather(response) {
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
-
 function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];    
-    let day = days[date.getDay()];
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-    return `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -46,7 +53,30 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+    forecastHtml + 
+      `         <div class="weather-forecast-day">
+                                <div class="weather-forecast-date">${day}</div>
+                                <div class="weather-forecast-icon">🌤️</div>
+                                <div class="weather-forecast-temperatures">
+                                    <div class="weather-forecast-temperature"><strong>15°</strong></div>
+                                    <div class="weather-forecast-temperature"> 9°</div>
+                                </div>
+                            </div>
+                            `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+displayForecast();
